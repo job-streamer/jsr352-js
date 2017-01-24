237900f8233ee4040b32fb0d748c11f018f0fcbe
Yuki Seki <schokolade0317@gmail.com>
2017-01-24 14:43:33 +0900

    enable dev-mode

--- app/util/ComponentProvider.js
+++ app/util/ComponentProvider.js
@@ -1,35 +1,39 @@
 'use strict';
 
 var edn = require('edn');
+//　TODO: It must set false when controlBus return 404 or 503
+var Control_bus_meta_data = document.querySelector("meta[name=control-bus-url]");
 //  TODO:now,it does not get environment value from server 
-var controlBusURL = document.querySelector("meta[name=control-bus-url]").getAttribute("content");
-var appName = "default";
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-var componentJSON;
-xhr.addEventListener("load", function (ev) {
-    componentJSON = edn.valueOf(edn.parse(xhr.responseText));
-});
-xhr.open("GET", controlBusURL + "/" + appName + "/batch-components");
-xhr.send();
+var controlBusURL = Control_bus_meta_data ? document.querySelector("meta[name=control-bus-url]").getAttribute("content") : "http://localhost:45102";
 
+var appName = "default";
+if (Control_bus_meta_data) {
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  var componentJSON;
+  xhr.addEventListener("load", function (ev) {
+    componentJSON = edn.valueOf(edn.parse(xhr.responseText));
+  });
+  xhr.open("GET", controlBusURL + "/" + appName + "/batch-components");
+  xhr.send();
+}
 function ComponentProvider() {
 }
 
 ComponentProvider.getBatchlets = function () {
-    return componentJSON["batch-component/batchlet"];
+    return Control_bus_meta_data ? componentJSON["batch-component/batchlet"] : [];
 };
 ComponentProvider.getItemReaders = function () {
-    return componentJSON["batch-component/item-reader"];
+    return Control_bus_meta_data ? componentJSON["batch-component/item-reader"] : [];
 };
 ComponentProvider.getItemProcessors = function () {
-    return componentJSON["batch-component/item-processor"];
+    return Control_bus_meta_data ? componentJSON["batch-component/item-processor"] : [];
 };
 ComponentProvider.getItemWriters = function () {
-    return componentJSON["batch-component/item-writer"];
+    return Control_bus_meta_data ? componentJSON["batch-component/item-writer"] : [];
 };
 ComponentProvider.getListeners = function () {
-    return componentJSON["batch-component/listener"];
+    return Control_bus_meta_data ? componentJSON["batch-component/listener"] : [];
 };
 
 module.exports = ComponentProvider;
